790c6446b71b5d5e28d879c8cc9749f0a7f60129
Gustavo Dias <128855647+euoguss@users.noreply.github.com>
2024-11-09 12:17:46 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,render_template, redirect,session
+from flask import Flask, request, render_template, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import bcrypt
 
